25329ee79832d7988e252d1d5bccd4b4c57ec734
kshurygin <k.shurygin@gmail.com>
2017-04-23 15:35:48 +0300

    All works + favicon

--- javascripts/sidebar.js
+++ javascripts/sidebar.js
@@ -3,7 +3,7 @@ $(function() {
     $(this).on('click', function(e) {
       e.preventDefault();
       var scrollTo = $(this).attr('href');
-      var whitespace = scrollTo == '#s1' ? 120 : 30
+      var whitespace = scrollTo == '#s1' ? 230 : 30 // 230 это величина отступа
       $('html,body').animate({
         scrollTop: $(scrollTo).offset().top - whitespace
       }, 1000);
